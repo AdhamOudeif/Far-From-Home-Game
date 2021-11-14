59627812c2ae0907ca76bee439fa50d674db909b
smick00 <smick@emich.edu>
2021-11-13 22:22:04 -0500

    Super basic horizontal movement + jumping

--- Assets/GameFiles/Scripts/Player.cs
+++ Assets/GameFiles/Scripts/Player.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
+    [SerializeField] private float JumpForce = 5f;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private LayerMask WhatIsGround;
+
     private Rigidbody2D rb;
+    private BoxCollider2D coll;
+    private float DirectionX = 0f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //left and right
+        var movement = Input.GetAxis("Horizontal");
+        //deltaTime stuff to smooth out movement
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * moveSpeed;
+
+        //jumping
+        //checks to see, firstly, if player is pressing space, AND that the player isn't already moving thru the air.
+        if(Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.0001)
+        {
+            rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
     }
 }
