07f240cebae6402f3dece8d4a37e1487b6accdca
Adham Oudeif <aoudeif5@emich.edu>
2021-11-16 12:22:13 -0500

    Completed animaitons, added physics, remapped some keys

--- Assets/GameFiles/Scripts/Player.cs
+++ Assets/GameFiles/Scripts/Player.cs
@@ -8,7 +8,10 @@ public class Player : MonoBehaviour
     [SerializeField] private float JumpForce = 5f;
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private LayerMask WhatIsGround;
-    public CharacterController2D controller;
+
+    private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+    private bool m_Grounded;            // Whether or not the player is grounded.
+    //public CharacterController2D controller;
     private Rigidbody2D rb;
     public Animator animator;
 
@@ -22,21 +25,54 @@ public class Player : MonoBehaviour
     {
         //left and right
         var movement = Input.GetAxis("Horizontal");
-	animator.SetFloat("Speed", Mathf.Abs(movement));
+	    animator.SetFloat("Speed", Mathf.Abs(movement));
+        // If the input is moving the player right and the player is facing left...
+        if (movement > 0 && !m_FacingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (movement < 0 && m_FacingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
         //deltaTime stuff to smooth out movement
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * moveSpeed;
 
         //jumping
-        //checks to see, firstly, if player is pressing space, AND that the player isn't already moving thru the air.
+        //checks to see, firstly, if player is pressing jump button, AND that the player isn't already moving thru the air.
         if(Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.0001)
         {
             rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
-	    animator.SetBool("isJumping", true);
+	        animator.SetBool("isJumping", true);
 
+        }else if(Mathf.Abs(rb.velocity.y) < 0.000001)
+        {
+            animator.SetBool("isJumping", false);
         }
+
+        //crouch
+        if (Input.GetButtonDown("Crouch"))
+        {
+            animator.SetBool("isCrouching", true);
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            animator.SetBool("isCrouching", false);
+        }
+
     }
-	
-    public void OnLanding(){
-	animator.SetBool("isJumping", false);
-	}
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
